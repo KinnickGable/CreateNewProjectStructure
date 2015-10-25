0342473e78a98b75be30d3e797ee2deba2b8b09c
unknown <MDONNELLY@DONNELLY-M-W8.corp.allscripts.com>
2015-10-25 12:25:32 -0500

    Initial Commit

---
+++ CreateNewProjectStructure.bat
@@ -0,0 +1,26 @@
+@if exist "C:\users\donnell2\desktop" goto rootok
+@echo ...
+@echo --- Error! The path provided does not exist: C:\users\donnell2\desktop
+@goto :endscript
+
+:rootok
+@echo ...
+@echo --- Creating Folders Structure
+cd "C:\users\donnell2\desktop"
+
+md "dbM Folder"
+
+
+cd "C:\users\donnell2\desktop\dbM Folder"
+md "Backups"
+md "Inbox"
+md "Configs"
+md "Tools"
+md "Project Packs"
+
+
+
+@echo ...
+@echo --- Finished!
+
+:endscript
